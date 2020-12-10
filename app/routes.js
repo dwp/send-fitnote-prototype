d8ea4103b7de8606aa8e0ba5551a8683f275a621
Aaron Wilson-Wilks <aaron.wilson-wilks@engineering.digital.dwp.gov.uk>
2020-12-10 12:08:49 +0000

    add file upload select route and pages

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,24 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.route('/v7a-three-options-select')
+  .post((req, res, next) => {
+    let redirectUrl
+    switch (req.body['obtained-fit-note']) {
+      case 'paper':
+        redirectUrl = 'v7a-fitnote-version-1-paper'
+        break
+      case 'sms':
+        redirectUrl = 'v7a-fitnote-version-2-sms'
+        break
+      case 'email':
+        redirectUrl = 'v7a-fitnote-version-3-email'
+        break
+      default:
+        redirectUrl = req.path
+        break
+    }
+    res.redirect(redirectUrl)
+  })
 
 module.exports = router
